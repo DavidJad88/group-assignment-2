3a28ab27d70db4b0c1af6ea479ec31f37cb57738
DavidJad88 <davidjad88@gmail.com>
2025-04-28 14:58:44 +0200

    add props to form to close modal, fix styles

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "../Button/Button";
 import styles from "./Form.module.css";
 
-const Form = () => {
+const Form = ({ isAddingPlant, setIsAddingPlant }) => {
   const [formData, setFormData] = useState({
     commonName: "",
     scientificName: "",
@@ -12,15 +12,17 @@ const Form = () => {
     temperatureRange: "",
     humidity: "",
     toxicity: "",
-  })
+  });
 
   const [error, setError] = useState(null);
 
-  const handleChange = (e)=>{
-    const {name, value} = e.target;
-    setFormData((prevData)=>(
-      {...prevData, [name]: value}
-    ))
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
+  const handleCancel = () => {
+    setIsAddingPlant(false);
   };
 
   return (
@@ -28,47 +30,95 @@ const Form = () => {
       <h2 className={styles.formTitle}>Add new plant</h2>
       <div className={styles.formGroup}>
         <label htmlFor="common-name">Common name</label>
-        <input type="text" name="commonName" id="common-name" onChange={handleChange}
-        value={formData.commonName}/>
+        <input
+          type="text"
+          name="commonName"
+          id="common-name"
+          onChange={handleChange}
+          value={formData.commonName}
+        />
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="scientific-name">Scientific name</label>
-        <input type="text" name="scientificName" id="scientific-name" onChange={handleChange}
-        value={formData.scientificName}/>
+        <input
+          type="text"
+          name="scientificName"
+          id="scientific-name"
+          onChange={handleChange}
+          value={formData.scientificName}
+        />
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="watering-schedule">Watering schedule</label>
-        <input type="text" name="wateringSchedule" id="watering-schedule" onChange={handleChange}
-        value={formData.wateringSchedule}/>
+        <input
+          type="text"
+          name="wateringSchedule"
+          id="watering-schedule"
+          onChange={handleChange}
+          value={formData.wateringSchedule}
+        />
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="light-requirement">Light requirement</label>
-        <input type="text" name="lightRequirement" id="light-requirement" onChange={handleChange}
-        value={formData.lightRequirement}/>
+        <input
+          type="text"
+          name="lightRequirement"
+          id="light-requirement"
+          onChange={handleChange}
+          value={formData.lightRequirement}
+        />
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="soil-type">Soil type</label>
-        <input type="text" name="soilType" id="soil-type" onChange={handleChange}
-        value={formData.soilType}/>
+        <input
+          type="text"
+          name="soilType"
+          id="soil-type"
+          onChange={handleChange}
+          value={formData.soilType}
+        />
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="temperature-range">Temperature range</label>
         <div className={styles.radioContainer}>
-          <input type="radio" name="temperatureRange" id="cold" value={"cold"} onChange={handleChange}></input>
+          <input
+            type="radio"
+            name="temperatureRange"
+            id="cold"
+            value={"cold"}
+            onChange={handleChange}
+          ></input>
           <label htmlFor="cold">Cold 5 - 15°C</label>
         </div>
         <div className={styles.radioContainer}>
-          <input type="radio" name="temperatureRange" id="temperate" value={"temperate"} onChange={handleChange}></input>
+          <input
+            type="radio"
+            name="temperatureRange"
+            id="temperate"
+            value={"temperate"}
+            onChange={handleChange}
+          ></input>
           <label htmlFor="temperate">Temperate 15 - 22°C</label>
         </div>
         <div className={styles.radioContainer}>
-          <input type="radio" name="temperatureRange" id="warm" value={"warm"} onChange={handleChange}></input>
+          <input
+            type="radio"
+            name="temperatureRange"
+            id="warm"
+            value={"warm"}
+            onChange={handleChange}
+          ></input>
           <label htmlFor="warm">Warm 22 - 30°C</label>
         </div>
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="humidity">Humidity</label>
-        <select name="humidity" id="humidity" onChange={handleChange} value={formData.humidity}>
+        <select
+          name="humidity"
+          id="humidity"
+          onChange={handleChange}
+          value={formData.humidity}
+        >
           <option value="">Select option</option>
           <option value="low">Low</option>
           <option value="medium">Medium</option>
@@ -77,25 +127,32 @@ const Form = () => {
       </div>
       <div className={styles.formGroup}>
         <label htmlFor="toxicity">Toxicity</label>
-        <select name="toxicity" id="toxicity" onChange={handleChange} value={formData.toxicity}>
+        <select
+          name="toxicity"
+          id="toxicity"
+          onChange={handleChange}
+          value={formData.toxicity}
+        >
           <option value="">Select option</option>
           <option value="toxic">Toxic</option>
           <option value="non-toxic">Non-toxic</option>
         </select>
       </div>
 
-      <div className={styles.formGroup}>
+      <div className={styles.uploadGroup}>
         <label htmlFor="imageUpload">
           Please upload an image of your plant
         </label>
         <input type="file" name="imageUpload" id="imageUpload" />
       </div>
       <div className={styles.buttonContainer}>
-        <Button className={styles.secondaryButton}>Cancel</Button>
+        <Button className={styles.secondaryButton} onClick={handleCancel}>
+          Cancel
+        </Button>
         <Button>Add</Button>
       </div>
     </form>
-  )
+  );
 };
 
 export default Form;
