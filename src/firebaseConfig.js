d81dc0a2da522b54fe5d270541fe92ecdc4d05b9
DavidJad88 <davidjad88@gmail.com>
2025-04-28 14:27:34 +0200

    set up basic firebaseConfig

---
+++ src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAUZWTM1-8Fpjmxw9KYNN5nkZ-2txhZWuI",
+  authDomain: "group-assignment-2-group2.firebaseapp.com",
+  projectId: "group-assignment-2-group2",
+  storageBucket: "group-assignment-2-group2.firebasestorage.app",
+  messagingSenderId: "58527051049",
+  appId: "1:58527051049:web:85aa04041156c50d271b38",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
